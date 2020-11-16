5e25e7f4dd71b46b97ae1372c5ec9272ebf2fd00
Adetunji Favour <58666102+Tunjii10@users.noreply.github.com>
2020-11-16 15:03:23 +0100

    multiple prediction testing

--- packages/classification_model/tests/test_predict.py
+++ packages/classification_model/tests/test_predict.py
@@ -16,3 +16,20 @@ def test_make_single_prediction():
 	assert subject is not None
 	assert math.ceil(subject.get('predictions')[0]) == 0
 
+	
+def test_make_multiple_predictions():
+    # Given
+    test_data = load_dataset(file_name='test.csv')
+    original_data_length = len(test_data)
+    multiple_test_input = test_data.to_json(orient='records')
+
+    # When
+    subject = make_prediction(input_data=multiple_test_input)
+
+    # Then
+    assert subject is not None
+    assert len(subject.get('predictions')) == 418
+
+    # We expect some rows to be filtered out
+    assert len(subject.get('predictions')) == original_data_length
+
